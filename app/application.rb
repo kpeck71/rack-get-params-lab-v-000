c837ac4c39589531022cd3d8fe1cfcfbea35f21d
Kristen Peck <kpeck71@gmail.com>
2017-12-04 01:20:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      new_item = req.params["item"]
+      new_item = req.params["q"]
       if @@items.include?(new_item)
         @@cart << new_item
         resp.write "added #{new_item}\n"
